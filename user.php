221cfdb70874dec628bf7bb1782b8d9a8b5c8a7e
Sara Dempsey <dempseys3@vcu.edu>
2021-04-18 19:28:26 -0400

    Started user page

---
+++ user.php
@@ -0,0 +1,24 @@
+<?php 
+
+require_once ('connection.php');
+
+$stmt = $conn->prepare("SELECT user_id, first_name, last_name, email, type, address, city, user_state, zip_code, birthday
+                        FROM users
+                        ORDER BY first_name, last_name");
+
+$stmt->execute();
+
+echo "<table style='border: solid 1px black;'>";
+echo "<thead><tr><th>ID</th><th>First name</th><th>Last name</th></tr></thead>";
+echo "<tbody>";
+
+while($row = $stmt->fetch()){
+    echo "<tr><td>$row[user_id]</td><td>$row[first_name]</td><td>$row[last_name]</td><td>$row[email]</td></td>$row[type]
+         </td><td>$row[address]</td><td>$row[city]</td><td>$row[user_state]</td><td>$row[zip_code]</td></tr>$row[birthday]";
+    
+}
+
+echo "</tbody>";
+echo "</table>";
+
+?>
