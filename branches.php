17311e48b36a0a69150dd364b6adacb1fe27158f
Sara Dempsey <dempseys3@vcu.edu>
2021-04-29 20:36:02 -0400

    Basic branches page

---
+++ branches.php
@@ -0,0 +1,22 @@
+<?php 
+
+require_once ('connection.php');
+
+$stmt = $conn->prepare("SELECT branch_id, CONCAT(street_address, ' ', city, ' ', branch_state, ' ', zip_code) AS 'Address'
+                        FROM branches");
+
+$stmt->execute();
+
+echo "<table style='border: solid 1px black;'>";
+echo "<thead><tr><th>Branch ID</th><th>Address</th></tr></thead>";
+echo "<tbody>";
+
+while($row = $stmt->fetch()){
+    echo "<tr><td>$row[branch_id]</td><td>$row[Address]</td></tr>";
+    
+}
+
+echo "</tbody>";
+echo "</table>";
+
+?>
