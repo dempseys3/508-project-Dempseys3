f33889abc95d8392d351b5971f222709b9ded333
Sara Dempsey <dempseys3@vcu.edu>
2021-04-29 20:18:28 -0400

    Basic schedule page and employee schedule table

---
+++ employees-schedules.php
@@ -0,0 +1,25 @@
+<?php 
+
+require_once ('connection.php');
+
+$stmt = $conn->prepare("SELECT e.employee_id, u.first_name, u.last_name, schedule_ID, es.type
+                        FROM employee_schedules e JOIN users u ON e.employee_id = u.user_id 
+                        JOIN employees es ON u.user_id = es.employee_id
+                        WHERE e.employee_id = es.employee_id
+                        ORDER BY e.employee_id");
+
+$stmt->execute();
+
+echo "<table style='border: solid 1px black;'>";
+echo "<thead><tr><th>ID</th><th>First name</th><th>Last name</th><th>Schedule ID</th><th>Type</th></tr></thead>";
+echo "<tbody>";
+
+while($row = $stmt->fetch()){
+    echo "<tr><td>$row[employee_id]</td><td>$row[first_name]</td><td>$row[last_name]</td><td>$row[schedule_ID]</td></td>$row[type]</td></tr>";
+    
+}
+
+echo "</tbody>";
+echo "</table>";
+
+?>
---
+++ schedules.php
@@ -0,0 +1,22 @@
+<?php 
+
+require_once ('connection.php');
+
+$stmt = $conn->prepare("SELECT schedule_ID, start_date, end_date
+                        FROM schedules");
+
+$stmt->execute();
+
+echo "<table style='border: solid 1px black;'>";
+echo "<thead><tr><th>Schedule ID</th><th>Start Date</th><th>End Date</th></tr></thead>";
+echo "<tbody>";
+
+while($row = $stmt->fetch()){
+    echo "<tr><td>$row[schedule_ID]</td><td>$row[start_date]</td><td>$row[end_date]</td></tr>";
+    
+}
+
+echo "</tbody>";
+echo "</table>";
+
+?>
