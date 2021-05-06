a38a66ce076413750b4a23caba740b13483cc33c
Sara Dempsey <dempseys3@vcu.edu>
2021-05-06 17:10:49 -0400

    make payment

---
+++ make-payment.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<body>
+<form action = "sign-up-user.php" method = "POST">
+		<input type = 'number' name = "amount" placeholder = "Amount">
+		<br>
+		<input type = 'text' name = "time" placeholder = "Time">
+		<br>
+		<input type = 'text' name = "id" placeholder = "6-Digit Member ID">
+		<br>
+		<input type = 'password' name = "pwd" placeholder = "Password">
+		<br>
+		<input type = 'text' name = "type" placeholder = "Type">
+		<br>
+		<input type = 'text' name = "address" placeholder ="Address">
+		<br>
+		<input type = 'text' name = "city" placeholder = "City">
+		<br>
+		<input type = 'text' name = "state" placeholder = "State">
+		<br>
+		<input type = 'text' name = "zip" placeholder = "Zip-Code">
+		<br>
+		<input type = 'text' name = "birthday" placeholder = "Birthday yyyy-mm-dd">
+		<button type = "submit" name= "submit">Sign Up</button>
+</form>
+</body>
+</head>
+</html>
