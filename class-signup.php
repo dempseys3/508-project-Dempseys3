2e5b16bf8cbf925ba4f52da4d29ab435557b4b56
Sara Dempsey <dempseys3@vcu.edu>
2021-05-05 20:33:57 -0400

    Class signup page

---
+++ class-signup.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<body>
+<form action = "class-signup2.php" method = "POST">
+		<input type = 'number' name = "id" placeholder = "6-Digit user ID">
+		<br>
+		<br>
+		<input type = 'text' name = "first" placeholder = "First Name">
+		<br>
+		<br>
+		<input type = 'text' name = "last" placeholder = "Last Name">
+		<br>
+		<br>
+		<input type = 'text' name = "email" placeholder = "Email">
+		<br>
+		<br>
+		<input type = 'text' name = "class" placeholder = "Class Name">
+		<br>
+		<button type = "submit" name= "submit">Sign Up</button>
+</form>
+</body>
+</head>
+</html>
